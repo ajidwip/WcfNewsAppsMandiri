1f995b5a1a278fb3a6fe2bf67d35c8f4851f53eb
Aji Dwi Pangestu <aji.pangestu@unichemindo.com>
2024-05-24 19:36:14 +0700

    first commit

---
+++ WcfTemp/IService1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfTemp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        Temp SP_GetCountry();
+
+        [OperationContract]
+        Temp SP_GetCategory();
+
+        [OperationContract]
+        Temp SP_GetNews(string country, string category, string query, string page);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Temp
+    {
+        [DataMember]
+        public DataTable Temptable
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    public class table
+    {
+        [DataMember]
+        public DataTable tabledata
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ WcfTemp/Service1.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfTemp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        DataTable dt = new DataTable();
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString);
+        Temp temp = new Temp();
+        table tabledt = new table();
+        public Temp SP_GetCountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("[SP_GetCountry]", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            dt = new DataTable("Paging");
+            da.Fill(dt);
+            temp.Temptable = dt;
+
+
+            con.Close();
+
+            return temp;
+        }
+        public Temp SP_GetCategory()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("[SP_GetCategory]", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            dt = new DataTable("Paging");
+            da.Fill(dt);
+            temp.Temptable = dt;
+
+
+            con.Close();
+
+            return temp;
+        }
+        public Temp SP_GetNews(string country, string category, string query, string page)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("[SP_GetNews] '" + country + "','" + category + "','" + query + "','" + page + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            dt = new DataTable("Paging");
+            da.Fill(dt);
+            temp.Temptable = dt;
+
+
+            con.Close();
+
+            return temp;
+        }
+
+    }
+}
